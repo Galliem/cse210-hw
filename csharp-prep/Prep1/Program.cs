a6c8a8f9df864c327e3faec7c88bf2fcc7cb99f8
galliem <gar22094@byui.edu>
2024-01-10 09:30:58 -0700

    changed net7.0 to net8.0

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,3 +7,8 @@ class Program
         Console.WriteLine("Hello Prep1 World!");
     }
 }
+string first_name = Console.WriteLine("What is your first name? ")
+Console.WriteLine("What is your last name? ")
+
+
+string last_name = Console.ReadLine();
