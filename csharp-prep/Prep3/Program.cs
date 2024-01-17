554965494617fc702af18b07d932ce1a96fccad9
galliem <gar22094@byui.edu>
2024-01-17 09:26:58 -0700

    practiced four types of loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        int count = 5;
+
+        while (count < 10) {
+            Console.WriteLine($"Count = {count}");
+            count += 1;
+        }
+
+        int anotherCount = 0;
+        do {
+            System.Console.WriteLine($"AnotherCount = {anotherCount++}");
+        } while (anotherCount < 10);
+
+        for (int i=0; i<5; ++i) {
+            System.Console.WriteLine($"i = {i}");
+        }
+        
     }
 }
