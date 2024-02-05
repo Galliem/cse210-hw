b84f3eee3716243d1f6026d8d4407a31247edebc
galliem <gar22094@byui.edu>
2024-02-05 09:21:07 -0700

    added program.cs for car program

---
+++ prepare/Learning03/CarAssignment/Program.cs
@@ -0,0 +1,10 @@
+namespace learning03;
+using system;
+class Program{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        var car = new char("Toyota", "Camry");
+        car.PublicDisplay;
+    }
+}
