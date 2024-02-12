f7d18930dd9f4185013366b567c642125ba5c968
galliem <gar22094@byui.edu>
2024-02-12 09:08:46 -0700

    added scriptureText

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        var scriptureText = 'Now behold, a marvelous work is about to come forth among the children of men. Therefore, O ye that embark in the service of God, see that ye serve him with all your heart, might, mind, and strength, that ye may stand blameless before God at the last day. Therefore, if ye have desires to serve God yea are called to the work; For behold the field is white already to harvest; and lo, he that thrusteth in his sickle with his might, the same layeth up in store that he perisheth not, but bringeth salvation to his soul; And faith, hope, charity and love, with an eye single to the glory of God, qualify him for the work. Remember that faith, virtue, knowledge, temperance, patience, brotherly kindness, godliness, charity, humility, dilligence. Ask and ye shall recieve; knock and it shall be opened unto you. Amen.';
+        Console.WriteLine(scriptureText);
     }
 }
